0410f1ff41930c29fc922b4a45b42ccbd6d63386
AnttiKeronen <antti.keronen@student.lut.fi>
2026-01-08 14:44:19 +0200

    pitäs olla

--- src/__test__/task3.test.tsx
+++ src/__test__/task3.test.tsx
@@ -1,21 +1,20 @@
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import MyList from "../components/MyList";
+import MyList, { TItem } from "../components/MyList"; 
 
 test("List takes props", () => {
-  let items = [
+  const items: TItem[] = [
     { id: "5", text: "item number 1", clicked: false },
     { id: "420", text: "item number 2", clicked: true },
   ];
 
-  // ✅ Use onItemClick, not updateList
-  let props = { 
+  const props = { 
     header: "Another header", 
     items: items, 
     onItemClick: jest.fn() 
   };
 
-  let { getByText } = render(<MyList {...props} />);
+  const { getByText } = render(<MyList {...props} />);
   
   expect(getByText(/another header/i)).toBeInTheDocument();
   expect(getByText(/item number 1/i)).toBeInTheDocument();
